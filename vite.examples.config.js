e208911921f6e95ffe8368c575d13fbc85d40e9a
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2025-06-05 20:00:15 +0100

    implemented multiple endpoints for dev example

---
+++ vite.examples.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+
+import { watchAndRun } from "vite-plugin-watch-and-run";
+import path from "node:path";
+
+export default defineConfig({
+  server: {
+    open: "./test/example/",
+  },
+  plugins: [
+    watchAndRun([
+      {
+        name: "Generating test assets.",
+        run: "npm run g && cp test/example/assets/style.css test/layouts/assets/style.css",
+        watch: path.resolve("src/**/*.(css|tinchi)"),
+      },
+    ]),
+  ],
+});
