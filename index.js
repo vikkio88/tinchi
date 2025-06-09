f4b9eea0b9c61cb562ffe675ea3002b178c57cf6
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2025-06-09 19:58:47 +0100

    - adding snippet generator - handling errors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import { getArgs } from './lib/getArgs.mjs';
-import { validate } from './lib/validate.mjs';
-import { METHODS } from './lib/methodMap.mjs';
+import { getArgs } from "./lib/getArgs.mjs";
+import { validate } from "./lib/validate.mjs";
+import { METHODS } from "./lib/methodMap.mjs";
 
 const { args, positionals } = getArgs(process.argv.slice(2));
 validate(args, positionals);
@@ -9,6 +9,11 @@ validate(args, positionals);
 const [methodName, ...otherPositionals] = positionals;
 
 const method = METHODS[methodName];
-
-method ? method(otherPositionals, args) : METHODS.help({ error: true, message: `method ${methodName} not found.` })
-
+try {
+  method
+    ? method(otherPositionals, args)
+    : METHODS.help({ error: true, message: `method ${methodName} not found.` });
+} catch (error) {
+  console.error(`Error whilst running '${methodName}' method:`, error.message);
+  process.exit(1);
+}
